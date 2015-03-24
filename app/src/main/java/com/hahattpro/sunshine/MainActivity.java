492a8d027447b3efaef23363a3cceac72b3172d2
ThaiThien <tthien@apcs.vn>
2015-03-24 09:21:28 +0700

    add arraylist

--- app/src/main/java/com/hahattpro/sunshine/MainActivity.java
+++ app/src/main/java/com/hahattpro/sunshine/MainActivity.java
@@ -11,6 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -60,6 +64,11 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> arrayList = new ArrayList<String>();
+            Collections.addAll(arrayList,getResources().getStringArray(R.array.forecast_example));
+                   
+
             return rootView;
         }
     }
